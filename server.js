ab95f5e19910f4a17a511dca104ceed7cae1e1cf
Abdullahi Idris <myrahama@gmail.com>
2022-10-27 18:26:23 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+﻿const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// allow cors requests from any origin and with credentials
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+
+// api routes
+app.use('/', intro);
+
+function intro(req, res, next){
+    return res.json({
+        slackUsername: "abdullahidris",
+        backend: true,
+        age: 21,
+        bio: "I'm Abdullahi Idris, a backend developer from Lagos, Nigeria"
+    });
+}
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log('Server listening on port ' + port));
